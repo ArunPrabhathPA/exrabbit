eff6985cd3a2362064c8730889a97824ccc3562e
ed.o <ed.o@example.com>
2013-09-25 17:14:35 +0900

    Added documentation for rabbit server

--- lib/rabbit_server.ex
+++ lib/rabbit_server.ex
@@ -1,29 +1,58 @@
 defmodule Rabbit.Server do
+  @moduledoc """
+  Elixir genServer for RabbitMq
+  """
   use GenServer.Behaviour
-  defrecord MqServer, host: '127.0.01', port: 5672, username: "guest", password: "guest", exchange: "default", queue: "test"
+  defrecord MqServer, host: '127.0.01', port: 5672, username: "guest", password: "guest", exchange: "demo", queue: "test"
 
+  @doc """
+  Starts the gen_server
+  """
   def start_link do
     :gen_server.start_link({:local, :rabbit}, __MODULE__, [], [])
   end
 
+  @doc """
+  Callback for gen_server start_link
+  """
   def init(args) do
     state = {}
     {:ok, state}
   end
 
+  @doc """
+  Synchronous call to start publisher
+  Replies :ok or :error
+  DONT USE IT DIRECT INSTEAD CALL start_publisher(:name)
+  """
   def handle_call({:publisher, connArgs}, _from, state) do
     handle_call_({:connect, connArgs, []}, _from, state)
   end
 
+  @doc """
+  Synchronous call to start consumer
+  Replies :ok or :error
+  DONT USE IT DIRECT INSTEAD CALL start_publisher(:consumer)
+  """
   def handle_call({:consumer, connArgs}, _from, state) do
     handle_call_({:connect, connArgs, [:subscribe]}, _from, state)
   end
 
+  @doc """
+  Synchronous call to Subscribe to receive message
+  Replies :ok or :error
+  DONT USE IT DIRECT INSTEAD CALL subscribe(self())
+  """
   def handle_call({:subscribe, pid}, from, state) do
     newstate = pid
     {:reply, :ok, newstate}
   end
 
+  @doc """
+  Synchronous call to Start connection with rabbitMQ
+  Replies {:ok, pid} or {:error, reason}
+  DONT USE IT DIRECT INSTEAD CALL start_publisher() or start_consumer()
+  """
   def handle_call_({:connect, connArgs, args}, _from, state) do
     [name, host, port, username, password, exchange, queue] = connArgs
     res = :bunnyc.start_link(name, {:network, host, port, {username, password}}, {exchange, queue, ""}, args, self())
@@ -41,10 +70,19 @@ defmodule Rabbit.Server do
   {:reply, :ok, state}
   end
 
+
+  @doc """
+  Message received when consumer is started succeffuly
+  noreply is sent
+  """
   def handle_info({:"basic.consume_ok", msg}, state) do
     {:noreply, state}
   end
 
+  @doc """
+  Messages are received from rabbitmq and passed to the subscribed process
+  noreply is sent
+  """
   def handle_info({{:"basic.deliver",_,_,_,_,queue}, {amqp_msg, msgInfo, {:json, msg}}}, state) do
    # :gproc.send({:p,:l,:subscribers_pool}, {:mq_msg, msg})
    :io.format("State is ~p~n queue is ~p~n amqp_msg is ~p~n msginfo is ~p~n", [msg, queue, amqp_msg, msgInfo])
@@ -52,25 +90,77 @@ defmodule Rabbit.Server do
    {:noreply, state}
   end
 
+  @doc """
+  Messages are received from rabbitmq and passed to the subscribed process
+  noreply is sent
+  """
   def handle_info({{:"basic.deliver",_,_,_,_,queue}, {amqp_msg, msgInfo, msg}}, state) do
-   # :gproc.send({:p,:l,:subscribers_pool}, {:mq_msg, msg})
    :io.format("State is ~p~n queue is ~p~n amqp_msg is ~p~n msginfo is ~p~n", [msg, queue, amqp_msg, msgInfo])
    state <- msg
    {:noreply, state}
   end
 
 
+  @doc """
+    Starts consumer or publisher
+    DO NOT CALL IT DIRECTLY USE start_publisher or start_consumer
+  """
+  def start_mq_(type, name, host, port, username, password, exchange, queue) do
+     :gen_server.call :rabbit, {type, [name, host, port, username, password, exchange, queue]}
+  end
+
+  @doc """
+  Starts the consumer
+
+    Rabbit.Server.start_consumer(name)
+
+    ## name can be any valid atom
+  """
   def start_consumer(name) do
     rabbitInfo = MqServer.new()
     :gen_server.call :rabbit, {:consumer, [name, rabbitInfo.host, rabbitInfo.port, rabbitInfo.username, rabbitInfo.password, rabbitInfo.exchange, rabbitInfo.queue]}
   end
 
+  @doc """
+  Starts the consumer
+
+    Rabbit.Server.start_consumer(name, host)
+
+    ## name can be any valid atom
+    ## host can be any valid character string (Make sure to provide host in single quotes not in double quotes)
+  """
   def start_consumer_test(name) do
     rabbitInfo = MqServer.new()
     :gen_server.call :rabbit, {:consumer, [name, rabbitInfo.host, rabbitInfo.port, rabbitInfo.username, rabbitInfo.password, rabbitInfo.exchange, "testing"]}
 
   end
 
+  @doc """
+  Starts the publisher. Publisher can be started by providing only name , However, in this case rest all the values will be default. following flavours of start_publisher are available.
+
+    Rabbit.Server.start_publisher(name)
+    Rabbit.Server.start_publisher(name, host)
+    Rabbit.Server.start_publisher(name, host, username)
+    Rabbit.Server.start_publisher(name, host, username, password)
+    Rabbit.Server.start_publisher(name, host, username, password, exchange)
+    Rabbit.Server.start_publisher(name, host, username, password, exchange, queue)
+
+    ## name can be any valid atom
+    ## host can be any valid character string (Make sure to provide host in single quotes not in double quotes)
+    ## port can be any valid positive integer
+    ## username can be any valid binary string
+    ## password can be any valid binary string
+    ## exchange can be any valid binary string
+    ## queue can be any valid binary string
+
+    Default Values:
+    host = '127.0.0.1'
+    port = 5672
+    username = "guest"
+    password = "guest"
+    exchange= "demo"
+    queue = "test"
+  """
   def start_publisher(name) do
     rabbitInfo = MqServer.new()
     start_mq_(:publisher, name, rabbitInfo.host, rabbitInfo.port, rabbitInfo.username, rabbitInfo.password, rabbitInfo.exchange, rabbitInfo.queue)
@@ -100,6 +190,32 @@ defmodule Rabbit.Server do
     rabbitInfo = MqServer.new(host: host, port: port, username: username, password: password, exchange: exchange, queue: queue)
     start_mq_(:publisher, name, rabbitInfo.host, rabbitInfo.port, rabbitInfo.username, rabbitInfo.password, rabbitInfo.exchange, rabbitInfo.queue)
   end
+@doc """
+  Starts the consumer. Consumer can be started by providing only name , However, in this case rest all the values will be default. following flavours of start_publisher are available.
+
+    Rabbit.Server.start_consumer(name)
+    Rabbit.Server.start_consumer(name, host)
+    Rabbit.Server.start_consumer(name, host, username)
+    Rabbit.Server.start_consumer(name, host, username, password)
+    Rabbit.Server.start_consumer(name, host, username, password, exchange)
+    Rabbit.Server.start_consumer(name, host, username, password, exchange, queue)
+
+    ## name can be any valid atom
+    ## host can be any valid character string (Make sure to provide host in single quotes not in double quotes)
+    ## port can be any valid positive integer
+    ## username can be any valid binary string
+    ## password can be any valid binary string
+    ## exchange can be any valid binary string
+    ## queue can be any valid binary string
+
+    Default Values:
+    host = '127.0.0.1'
+    port = 5672
+    username = "guest"
+    password = "guest"
+    exchange= "demo"
+    queue = "test"
+  """
 
   def start_consumer(name) do
     rabbitInfo = MqServer.new()
@@ -131,18 +247,26 @@ defmodule Rabbit.Server do
     start_mq_(:consumer, name, rabbitInfo.host, rabbitInfo.port, rabbitInfo.username, rabbitInfo.password, rabbitInfo.exchange, rabbitInfo.queue)
   end
 
-  def start_mq_(type, name, host, port, username, password, exchange, queue) do
-    :gen_server.call :rabbit, {type, [name, host, port, username, password, exchange, queue]}
-  end
 
+  @doc """
+    publishes the message to exchange
+
+      Rabbit.Server.publish(pub, message)
+
+      pub is the atom which was provided while starting publisher
+      message is the text needs to be published
+  """
   def publish(publisher, message) do
     :bunnyc.publish(publisher, "", message)
   end
 
-  def publish(publisher, queue, message) do
-    :bunnyc.publish(publisher, queue, message)
-  end
+  @doc """
+    subscribes to receive mesage
 
+      Rabbit.Server.subscribe(pid)
+
+      pid is a valid process id. Most of the cases it will be self()
+  """
   def subscribe(pid) do
     :gen_server.call :rabbit, {:subscribe, pid}
   end
